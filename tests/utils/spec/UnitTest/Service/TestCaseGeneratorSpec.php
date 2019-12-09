317302d737b4507412c53052b6a5c436aed1b61e
Barney Hanlon <barney.hanlon@worldfirst.com>
2019-12-09 22:56:19 +0000

    Create matcher for test case and test generator

---
+++ tests/utils/spec/UnitTest/Service/TestCaseGeneratorSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Tests\Utils\UnitTest\Service;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use SplFileInfo;
+use Tests\Utils\Matcher\MatchTestCase;
+use Tests\Utils\UnitTest\ClassNamespace;
+use Tests\Utils\UnitTest\Service\NamespaceMapper\NamespaceMapperInterface;
+use Tests\Utils\UnitTest\Service\Pathalizer\PathalizerInterface;
+use Tests\Utils\UnitTest\TestCase;
+use Tests\Utils\UnitTestBundle\Message\CreateUnitTestMessage;
+
+final class TestCaseGeneratorSpec extends ObjectBehavior
+{
+    public function getMatchers(): array
+    {
+        return [
+            MatchTestCase::NAME => new MatchTestCase(),
+        ];
+    }
+
+    public function it_returns_a_test_case(
+        PathalizerInterface $pathalizer,
+        NamespaceMapperInterface $namespaceMapper
+    ): void {
+
+        $subjectFqn = 'Some\Subject';
+        $testCaseFqn = 'Tests\Utils\SubjectTest';
+
+        $fileInfo = new SplFileInfo(__FILE__);
+
+        $namespaceMapper->getTestCaseFqnFor(Argument::type(ClassNamespace::class))
+            ->willReturn(ClassNamespace::fromNamespaceString($testCaseFqn));
+
+        $pathalizer->getUnitTestPathFor($testCaseFqn)->willReturn($fileInfo);
+
+        $this->beConstructedWith($pathalizer, $namespaceMapper);
+        $this->createTestFor($subjectFqn)->shouldMatchTestCase(
+            $subjectFqn,
+            $testCaseFqn,
+            __FILE__
+        );
+    }
+}
---
+++ tests/utils/src/Matcher/MatchTestCase.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Utils\Matcher;
+
+
+use PhpSpec\Exception\Example\MatcherException;
+use Tests\Utils\UnitTest\ClassNamespace;
+use Tests\Utils\UnitTest\TestCase;
+
+final class MatchTestCase
+{
+    public const NAME = 'matchTestCase';
+
+    /**
+     * @param TestCase $testCase
+     * @param string $subjectFqn
+     * @param string $testCaseFqn
+     * @param string $path
+     * @return bool
+     * @throws MatcherException
+     */
+    public function __invoke(
+        TestCase $testCase,
+        string $subjectFqn,
+        string $testCaseFqn,
+        string $path
+    ): bool {
+        $testCaseClassNs = $testCase->getTestCaseFqn();
+        if (!$this->matchFqn($testCaseClassNs, $testCaseFqn)) {
+            throw new MatcherException(sprintf(
+                'Test case FQN "%s" does not match expected "%s"',
+                $testCaseClassNs->toString(),
+            $testCaseFqn
+            ));
+        }
+        $subjectClassNs = $testCase->getSubjectFqn();
+        if (!$this->matchFqn($subjectClassNs, $subjectFqn)) {
+            throw new MatcherException(sprintf(
+                'Subject FQN "%s" does not match expected "%s"',
+                $subjectClassNs->toString(),
+                $subjectFqn
+            ));
+        }
+
+        $fileInfo = $testCase->getFileInfo();
+
+        if ($fileInfo->getPathname() !== $path) {
+            throw new MatcherException(sprintf(
+                'Test case path "%s" does not match expected value "%s"',
+                $fileInfo->getPathname(),
+                $path
+            ));
+        }
+
+        return true;
+    }
+
+    /**
+     * @param ClassNamespace $actual
+     * @param string $expected
+     * @return bool
+     */
+    private function matchFqn(ClassNamespace $actual, string $expected): bool
+    {
+        return $actual->toString() === $expected;
+    }
+}
